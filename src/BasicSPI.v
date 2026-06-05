de3ccc90efdc5d0fefd56a6f0faf3401f8ef9115
Timo Braungardt <timo.braungardt@web.de>
2026-06-05 16:10:53 +0200

    add read to SPI FSM (half broken)

--- src/BasicSPI.v
+++ src/BasicSPI.v
@@ -25,7 +25,13 @@ assign SerialIn =            io_ManagerSerialIn;
 // Logic stuff
 reg  [7:0] opcode;
 reg [23:0] address;
-integer state;
+reg        write_address;
+reg        write_data;
+reg        read_data;
+integer    num_bits;
+integer    state;
+
+reg  [7:0] buffer [0:15];
 
 // states
 parameter integer idle          = 0;
@@ -46,6 +52,12 @@ initial begin : setup_registers
     SerialOut   <= 0;
     state       <= 0;
     o_ChipSelect_neg <= 1'b1;
+
+    // the default case is reading from an address.
+    write_address   <= 1;
+    write_data      <= 0;
+    read_data       <= 1;
+    num_bits        <= 31;
 end
 
 integer count = 0;
@@ -71,7 +83,12 @@ always @(negedge clk) begin : sm_logic
             count <= count -1;
             if (count == 0) begin
                 count <= 23; // otherwise underflow - can this be synthesised elegantly?
-                state <= send_address;
+                if (write_address)
+                    state <= send_address;
+                else if (write_data)
+                    state <= send_data;
+                else 
+                    state <= idle;
             end
         end
 
@@ -80,17 +97,37 @@ always @(negedge clk) begin : sm_logic
             count <= count -1;
 
             if (count == 0) begin
-                count <= 31;
-                state <= recieve_data;
+                count <= num_bits;
+                if (write_data)
+                    state <= send_data;
+                else
+                    state <= recieve_data;
             end
         end
 
+        // ToDo: the address has to be taken into account
         recieve_data : begin
             count <= count -1;
+
+            buffer[count[6:3]][count[2:0]] <= SerialIn;    // reads into the higher bytes first
+
             if (count == 0) begin
                 count <= 0; // otherwise underflow - can this be synthesised elegantly?
                 state <= idle;
-                o_ChipSelect_neg <= 1'b1;
+                enClk <= 1'b0;
+            end
+        end
+
+        // ToDo: the address has to be taken into account
+        send_data : begin
+            count <= count -1;
+
+            SerialOut <= buffer[count[6:3]][count[2:0]];   // writes the higher bytes first
+
+            if (count == 0) begin
+                count <= 0; // otherwise underflow - can this be synthesised elegantly?
+                state <= idle;
+                enClk <= 1'b0;
             end
         end 
 
--- test/cocotb/test_basic_spi.py
+++ test/cocotb/test_basic_spi.py
@@ -23,11 +23,17 @@ class SimpleSpiSubordinate(SpiSlaveBase):
         self.idle.clear()
         self.opcode = int(await self._shift(8, tx_word=(0x00)))
         self.address = int(await self._shift(24, tx_word=(0x888888)))
+
+        # Manager ordered a read
+        if self.opcode == 0x03:
+            await self._shift(32, 0x12345678)
+            #await self._shift(8, 0x78)
+
         await frame_end
 
 
 @cocotb.test()
-async def my_first_test(dut):
+async def transmission_test(dut):
     spi_subordinate = SimpleSpiSubordinate(
                     SpiBus(
                         entity=dut, 
@@ -52,6 +58,46 @@ async def my_first_test(dut):
     [opcode, address] = await spi_subordinate.get_content()
     assert opcode == 5
     assert address == 20
+
+
+@cocotb.test()
+async def read_test(dut):
+
+    spi_subordinate = SimpleSpiSubordinate(
+                    SpiBus(
+                        entity=dut, 
+                        sclk_name='o_SpiClk', 
+                        mosi_name='io_ManagerSerialOut', 
+                        miso_name='io_ManagerSerialIn', 
+                        cs_name='o_ChipSelect_neg')
+                    )
+    
+    dut.opcode.value = 0x03
+    dut.address.value = 20
+    
+    dut.write_address.value = 0b1
+    dut.write_data.value = 0b0
+    dut.read_data.value = 0b1
+    dut.num_bits.value = 33  # has to be +2 because we sample on the next edge, not on the "sending" edge!
+    # ToDo: the DUT is not sampling on the positive clock edge but the negative - fix!
+
+    c = Clock(dut.clk  , 20, 'ns')
+    cocotb.start_soon(c.start())
+
+    dut.go.value = 0
+    await cocotb.triggers.ClockCycles(dut.clk, 5, rising=True)
+    dut.go.value = 1
+    await cocotb.triggers.ClockCycles(dut.clk, 1, rising=True)
+    dut.go.value = 0
+    await cocotb.triggers.ClockCycles(dut.clk, 100, rising=True)
+    [opcode, address] = await spi_subordinate.get_content()
+
+    assert opcode == 3
+    assert address == 20
+    assert dut.buffer[0].value.integer == 0x12
+    assert dut.buffer[1].value.integer == 0x34
+    assert dut.buffer[2].value.integer == 0x56
+    assert dut.buffer[3].value.integer == 0x78
 
 
 def test_basic_spi():
