82367815c6e462a1902d1be0b09597d064bf4f26
Timo Braungardt <timo.braungardt@web.de>
2026-07-15 14:52:42 +0200

    spi: update basic spi to be similar to the hyperbus

--- src/BasicSPI.v
+++ src/BasicSPI.v
@@ -5,7 +5,7 @@ module BasicSPI (
     input go,
 
     // SPI Pins
-    output reg o_bus_clock,
+    output o_bus_clock,
     output reg o_chip_select_neg,
     output o_reset,
     inout io_manager_serial_in,
@@ -35,11 +35,11 @@ module BasicSPI (
     integer        count = 0;
     integer        clock_count = 0;
     reg            bus_clock = 0;
-    reg            clock_tick_pos = 0;
-    reg            clock_tick_neg = 0;
+    wire           clock_tick_pos;
+    wire           clock_tick_neg;
     integer        buffer_count = 0;
 
-    reg     [ 7:0] buffer             [0:BUFFER_SIZE -1];
+    reg     [ 7:0] buffer           [0:BUFFER_SIZE -1];
 
     // states
     localparam integer idle = 0;
@@ -48,6 +48,7 @@ module BasicSPI (
     localparam integer send_opcode = 1;
     localparam integer send_address = 2;
     localparam integer send_data = 3;
+    localparam integer send_data_setup = 12;
     localparam integer recieve_data = 4;
 
     // constants
@@ -63,7 +64,6 @@ module BasicSPI (
         serial_out        = 0;
         state             = 0;
         o_chip_select_neg = 1'b1;
-        o_bus_clock       = 1'b0;
 
         // the default case is reading from an address.
         write_address     = 1;
@@ -73,26 +73,21 @@ module BasicSPI (
     end
 
 
-    assign en_bus_clock = (state != idle) ? 1'b1 : 1'b0;
-    always @(posedge clk) begin : spi_clock
-        o_bus_clock <= bus_clock;
+    assign en_bus_clock   = (state != idle && state != cl_high) ? 1'b1 : 1'b0;
+    assign clock_tick_pos = (clock_count == 0 && ~bus_clock) ? 1'b1 : 1'b0;
+    assign clock_tick_neg = (clock_count == 0 && bus_clock) ? 1'b1 : 1'b0;
 
-        if (~en_bus_clock) begin
-            bus_clock      <= 1'b0;
-            clock_count    <= TIMER_COUNT;
-            clock_tick_pos <= 1'b0;
-            clock_tick_neg <= 1'b0;
+    assign o_bus_clock    = (en_bus_clock) ? bus_clock : 1'b0;
+
+    always @(posedge clk) begin : spi_clock
+        if (state == idle) begin
+            bus_clock   <= 1'b0;
+            clock_count <= TIMER_COUNT;
         end else begin
             if (clock_count == 0) begin
-                clock_count    <= TIMER_COUNT;
-                clock_tick_pos <= ~bus_clock;
-                clock_tick_neg <= bus_clock;
-                bus_clock      <= ~bus_clock;
-            end else begin
-                clock_count    <= clock_count - 1;
-                clock_tick_pos <= 1'b0;
-                clock_tick_neg <= 1'b0;
-            end
+                clock_count <= TIMER_COUNT;
+                bus_clock   <= ~bus_clock;
+            end else clock_count <= clock_count - 1;
         end
     end
 
@@ -135,10 +130,10 @@ module BasicSPI (
 
                 if (clock_tick_neg) count <= count - 1;
 
-                if (count == 0 && clock_tick_pos) begin
+                if (count == 0 && clock_tick_neg) begin
                     count <= num_bits - 1;
                     buffer_count <= 0;
-                    if (write_data) state <= send_data;
+                    if (write_data) state <= send_data_setup;
                     else if (read_data) state <= recieve_data;
                     else state <= cl_high;
                 end
@@ -163,6 +158,15 @@ module BasicSPI (
             end
 
 
+            send_data_setup: begin
+                count <= count - 1;
+                buffer_count <= buffer_count + 1;
+                serial_out <= buffer[buffer_count[6:3]][count[2:0]];
+                state <= send_data;
+            end
+
+            // ToDo: the problem is, that the count is 0, we shift out the new value and then go to the next state, which deactivates the output and the clock
+            // we would have to wait one more clock to output the last bit.
             send_data: begin
                 if (clock_tick_neg) begin
                     count <= count - 1;
@@ -170,7 +174,9 @@ module BasicSPI (
                     serial_out <= buffer[buffer_count[6:3]][count[2:0]];
                 end
 
-                if (count == 0 & clock_tick_neg) begin
+                // ToDo: this is needed, otherwise the last bit is not transfered
+                // this is not nice, but the file will hopefully be rewritten soon anyways...
+                if (count == -1 & clock_tick_neg) begin
                     count <= 0;  // otherwise underflow - can this be synthesised elegantly?
                     state <= cl_high;
                 end
@@ -178,9 +184,8 @@ module BasicSPI (
 
 
             cl_high: begin
-                if (clock_tick_neg) begin
+                if (clock_tick_pos) begin
                     state <= idle;
-                    o_chip_select_neg <= 1'b1;
                     en_serial_out <= 1'b0;
                 end
             end
