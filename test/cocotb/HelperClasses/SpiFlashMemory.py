7532a7a1cd569597068e04adaa516bf5048a3a0f
Timo Braungardt <timo.braungardt@web.de>
2026-07-16 16:53:50 +0200

    spi: rename cocotb helper functions

--- test/cocotb/HelperClasses/SpiFlashMemory.py
+++ test/cocotb/HelperClasses/SpiFlashMemory.py
@@ -22,26 +22,26 @@ class SpiFlashMemory(SpiSlaveBase):
         await self.idle.wait()
         return self.opcode, self.address
 
-    async def _read_data(self, num_bits: int) -> int:
+    async def _recieve_data(self, num_bits: int) -> int:
         rx_word = 0
 
         frame_end = RisingEdge(self._cs) if self._config.cs_active_low else FallingEdge(self._cs)
 
         for k in range(num_bits):
             if (await First(RisingEdge(self._sclk), frame_end)) == frame_end or self._cs.value == 1:
-                raise SpiFrameError("End of frame in the middle of a transaction")
+                raise RuntimeError("End of frame in the middle of a transaction")
 
             rx_word |= int(self._mosi.value) << (num_bits - 1 - k)
         return rx_word
 
-    async def _write_data(self, num_bits: int, tx_word: int) -> int:
+    async def _send_data(self, num_bits: int, tx_word: int) -> int:
         frame_end = RisingEdge(self._cs) if self._config.cs_active_low else FallingEdge(self._cs)
 
         for k in range(num_bits):
             if (
                 await First(FallingEdge(self._sclk), frame_end)
             ) == frame_end or self._cs.value == 1:
-                raise SpiFrameError("End of frame in the middle of a transaction")
+                raise RuntimeError("End of frame in the middle of a transaction")
 
             self._miso.value = bool(tx_word & (1 << (num_bits - 1 - k)))
 
@@ -49,18 +49,18 @@ class SpiFlashMemory(SpiSlaveBase):
         await frame_start
         self.log.info("SPI transaction started!")
         self.idle.clear()
-        self.opcode = int(await self._read_data(8))
+        self.opcode = int(await self._recieve_data(8))
         self.log.info("   opcode:  %x", self.opcode)
         if self.opcode == SpiFlashMemory.write_enable:
             self.write_enable = True
             self.log.info("   writing enabled")
         else:
-            self.address = int(await self._read_data(24))
+            self.address = int(await self._recieve_data(24))
             self.log.info("   address: %d", self.address)
 
         # Manager ordered a read
         if self.opcode == SpiFlashMemory.read:
-            await self._write_data(
+            await self._send_data(
                 32, tx_word=0x12345678
             )  # ToDo: always shifts out 4 bytes, change logic
             self.log.info("   reading data %x", self.data)
@@ -68,7 +68,7 @@ class SpiFlashMemory(SpiSlaveBase):
         # Manager ordered a program
         if self.opcode == SpiFlashMemory.program:
             self.data = int(
-                await self._read_data(16)
+                await self._recieve_data(16)
             )  # ToDo: only reads two bytes, change to an array
             self.log.info("   sending Data")
 
