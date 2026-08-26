40063fd8250f0dc4544bdac98d33c348833aa02b
Timo Braungardt <timo.braungardt@web.de>
2026-08-26 15:27:50 +0200

    spi: add quad read test on memory model

--- test/cocotb/test_combined_spi_model.py
+++ test/cocotb/test_combined_spi_model.py
@@ -9,14 +9,20 @@ from cocotb.clock import Clock
 from cocotb.types import Logic, LogicArray
 from unittest import SkipTest
 
+T_pp_typ = Timer(480, unit="us")    # program time typical from the datasheet
+T_w_typ  = Timer( 44, unit="ms")    # Nonvolatile Register Write Time
 
 class SPI_COMMANDS:
     write_enable = 0x06
+    write_enable_volatile = 0x50
+    write_any_reg = 0x71
     program = 0x02
     read = 0x03
+    read_114 = 0x6B
     read_status_reg_1 = 0x05
     read_status_reg_2 = 0x07
     read_config_reg_1 = 0x35
+    read_config_regs  = 0x65
     read_JEDEC_parameter = 0x5A
 
 
@@ -56,7 +62,7 @@ async def read_test(dut):
     dut.Controller.write_address.value = 0b0
     dut.Controller.write_data.value = 0b0
     dut.Controller.read_data.value = 0b0
-    dut.Controller.is_quad_mode.value = False
+    dut.Controller.is_quad_mode.value = 0b000
 
     await trigger_go(dut)
     await wait_for_idle(dut)
@@ -75,16 +81,11 @@ async def read_test(dut):
     dut.Controller.buffer[4].value = 0xFF
     dut.Controller.num_bits.value = 32
 
-    dut.go.value = 0
-    await cocotb.triggers.ClockCycles(dut.clk, 5, rising=True)
-    dut.go.value = 1
-    await cocotb.triggers.ClockCycles(dut.clk, 1, rising=True)
-    dut.go.value = 0
-    await cocotb.triggers.ClockCycles(dut.clk, 2, rising=True)
-    await dut.chip_select_neg.value_change
+    await trigger_go(dut)
+    await wait_for_idle(dut)
 
     # read
-    await Timer(480, unit="us")  # T_PP typ in the datasheet
+    await T_pp_typ
 
     dut.Controller.opcode.value = SPI_COMMANDS.read
     dut.Controller.address.value = 0x20
@@ -119,7 +120,7 @@ async def get_jedec_parameter_test(dut):
 
     dut.Controller.opcode.value = SPI_COMMANDS.read_JEDEC_parameter
     dut.Controller.address.value = 0x000000
-    dut.Controller.is_quad_mode.value = False
+    dut.Controller.is_quad_mode.value = 0b000
 
     dut.Controller.write_address.value = 0b1
     dut.Controller.write_data.value = 0b0
@@ -207,7 +208,7 @@ async def get_status_register_test(dut):
 
     dut.Controller.opcode.value = SPI_COMMANDS.read_status_reg_1
     dut.Controller.address.value = 0x000000
-    dut.Controller.is_quad_mode.value = False
+    dut.Controller.is_quad_mode.value = 0b000
 
     dut.Controller.write_address.value = 0b0
     dut.Controller.write_data.value = 0b0
@@ -273,6 +274,132 @@ async def get_status_register_test(dut):
 
     assert dut.Controller.buffer[0].value[0] == True  # Device Ready/Busy Status Flag
     assert dut.Controller.buffer[0].value[1] == True  # Write/Program Enable Status Flag
+
+
+@cocotb.test()
+async def qspi_read_test(dut):
+    c = Clock(dut.clk, 20, "ns")
+    cocotb.start_soon(c.start())
+    await reset_model(dut)
+
+    # check state QUADIT bit in config register 1
+    dut.Controller.opcode.value = SPI_COMMANDS.read_config_reg_1
+    dut.Controller.address.value = 0x000000
+    dut.Controller.is_quad_mode.value = 0b000
+    dut.Controller.write_address.value = 0b0
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b1
+    dut.Controller.num_bits.value = 8
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    assert dut.Controller.buffer[0].value[1] == False  # QUADIT bit
+
+    # write enable
+    dut.Controller.opcode.value = SPI_COMMANDS.write_enable
+    dut.Controller.write_address.value = 0b0
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b0
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    # set QUADIT bit in config register 1
+    dut.Controller.opcode.value = SPI_COMMANDS.write_any_reg
+    dut.Controller.address.value = 0x800002
+    # this is the volatile register
+    # The non-volatile is address 0x000002, but needs T_w_typ
+
+    dut.Controller.write_address.value = 0b1
+    dut.Controller.write_data.value = 0b1
+    dut.Controller.read_data.value = 0b0
+    dut.Controller.num_bits.value = 8
+    dut.Controller.buffer[0].value |= LogicArray(0b00000010, 8)    # set QUADIT bit to 1
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+    await T_pp_typ
+
+    # check status
+    dut.Controller.opcode.value = SPI_COMMANDS.read_status_reg_1
+    dut.Controller.write_address.value = 0b0
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b1
+    dut.Controller.num_bits.value = 8
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    assert dut.Controller.buffer[0].value[0] == False  # Device not busy
+    assert dut.Controller.buffer[0].value[1] == False  # Write is enabled
+    assert dut.Controller.buffer[0].value[5] == False  # Last erase operation was successful
+    assert dut.Controller.buffer[0].value[6] == False  # Last programming operation was successful
+
+    # check state QUADIT bit in config register 1
+    dut.Controller.opcode.value = SPI_COMMANDS.read_config_reg_1
+
+    dut.Controller.write_address.value = 0b0
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b1
+    dut.Controller.num_bits.value = 8
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    assert dut.Controller.buffer[0].value[1] == True  # QUADIT bit
+    dut.Controller.is_quad_mode.value = 0b001
+
+    # write enable
+    dut.Controller.opcode.value = SPI_COMMANDS.write_enable
+
+    dut.Controller.write_address.value = 0b0
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b0
+    dut.Controller.is_quad_mode.value = 0b000
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    # program
+    dut.Controller.opcode.value = SPI_COMMANDS.program
+    dut.Controller.address.value = 0x30
+
+    dut.Controller.write_address.value = 0b1
+    dut.Controller.write_data.value = 0b1
+    dut.Controller.read_data.value = 0b0
+    dut.Controller.buffer[0].value = 0x12
+    dut.Controller.buffer[1].value = 0x34
+    dut.Controller.buffer[2].value = 0x56
+    dut.Controller.buffer[3].value = 0x78
+    dut.Controller.buffer[4].value = 0xFF
+    dut.Controller.num_bits.value = 32
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+    await T_pp_typ
+
+    # read with 1-1-4
+    dut.Controller.opcode.value = SPI_COMMANDS.read_114
+    dut.Controller.num_dummy_cycles.value = 8
+    dut.Controller.address.value = 0x30
+
+    for i in range(8):
+        dut.Controller.buffer[i].value = 0
+
+    dut.Controller.write_address.value = 0b1
+    dut.Controller.write_data.value = 0b0
+    dut.Controller.read_data.value = 0b1
+    dut.Controller.num_bits.value = 32
+    dut.Controller.is_quad_mode.value = 0b001
+
+    await trigger_go(dut)
+    await wait_for_idle(dut)
+
+    assert dut.Controller.buffer[0].value.to_unsigned() == 0x12
+    assert dut.Controller.buffer[1].value.to_unsigned() == 0x34
+    assert dut.Controller.buffer[2].value.to_unsigned() == 0x56
+    assert dut.Controller.buffer[3].value.to_unsigned() == 0x78
 
 
 def test_combined_spi_model(wave=False):
