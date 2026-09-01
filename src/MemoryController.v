3c5b96a9a22851831690daa281aae8e640fcbf2e
Timo Braungardt <timo.braungardt@web.de>
2026-09-01 16:16:13 +0200

    axi: add base file for memory controller

---
+++ src/MemoryController.v
@@ -0,0 +1,140 @@
+`timescale 1ns / 100ps
+
+module MemoryController #(
+    parameter ADDR_WIDTH = 24,
+    parameter DATA_WIDTH = 32,
+    parameter STRB_WIDTH = (DATA_WIDTH / 8),
+    parameter ID_WIDTH   = 8
+
+) (
+    input clk,
+    input reset_neg,
+
+    // SPI Pins
+    output o_spi_bus_clock,
+    output o_spi_chip_select_neg,
+    output o_spi_reset,
+    inout  io_spi_data0_manager_serial_out,
+    inout  io_spi_data1_manager_serial_in,
+    inout  io_spi_data2,
+    inout  io_spi_data3,
+
+    // AXI Pins
+    input  wire [  ID_WIDTH-1:0] s_axi_awid,
+    input  wire [ADDR_WIDTH-1:0] s_axi_awaddr,
+    input  wire [           7:0] s_axi_awlen,
+    input  wire [           2:0] s_axi_awsize,
+    input  wire [           1:0] s_axi_awburst,
+    input  wire                  s_axi_awlock,
+    input  wire [           3:0] s_axi_awcache,
+    input  wire [           2:0] s_axi_awprot,
+    input  wire                  s_axi_awvalid,
+    output wire                  s_axi_awready,
+
+    input  wire [DATA_WIDTH-1:0] s_axi_wdata,
+    input  wire [STRB_WIDTH-1:0] s_axi_wstrb,
+    input  wire                  s_axi_wlast,
+    input  wire                  s_axi_wvalid,
+    output wire                  s_axi_wready,
+
+    output wire [ID_WIDTH-1:0] s_axi_bid,
+    output wire [         1:0] s_axi_bresp,
+    output wire                s_axi_bvalid,
+    input  wire                s_axi_bready,
+
+    input  wire [  ID_WIDTH-1:0] s_axi_arid,
+    input  wire [ADDR_WIDTH-1:0] s_axi_araddr,
+    input  wire [           7:0] s_axi_arlen,
+    input  wire [           2:0] s_axi_arsize,
+    input  wire [           1:0] s_axi_arburst,
+    input  wire                  s_axi_arlock,
+    input  wire [           3:0] s_axi_arcache,
+    input  wire [           2:0] s_axi_arprot,
+    input  wire                  s_axi_arvalid,
+    output wire                  s_axi_arready,
+
+    output wire [  ID_WIDTH-1:0] s_axi_rid,
+    output wire [DATA_WIDTH-1:0] s_axi_rdata,
+    output wire [           1:0] s_axi_rresp,
+    output wire                  s_axi_rlast,
+    output wire                  s_axi_rvalid,
+    input  wire                  s_axi_rready
+);
+
+
+    SPIController #(
+        .ADDRESS_LENGTH(ADDR_WIDTH),
+        .DATA_WIDTH(DATA_WIDTH)
+    ) SPI_Controller (
+        .clk(clk),
+        .reset_neg(reset_neg),
+        .go(),
+
+        .i_address(),
+        .i_write_enable(),
+        .i_last_word(),
+        .i_num_bytes(),
+        .i_data_write(),
+        .o_data_read(),
+        .o_busy(),
+        .o_next_word(),
+
+        // SPI Pins
+        .o_bus_clock(o_spi_bus_clock),
+        .o_chip_select_neg(o_spi_chip_select_neg),
+        .o_reset(),
+        .io_data0_manager_serial_out(io_spi_data0_manager_serial_out),
+        .io_data1_manager_serial_in(io_spi_data1_manager_serial_in),
+        .io_data2(io_spi_data2),
+        .io_data3(io_spi_data3)
+    );
+
+
+    AXI #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .ADDR_WIDTH(ADDR_WIDTH),
+        .ID_WIDTH  (ID_WIDTH),
+        .STRB_WIDTH(STRB_WIDTH)
+    ) AXI_Interface (
+        .clk(clk),
+        .rst(~reset_neg), // ToDo: make reset inverted
+
+        .s_axi_awid(s_axi_awid),
+        .s_axi_awaddr(s_axi_awaddr),
+        .s_axi_awlen(s_axi_awlen),
+        .s_axi_awsize(s_axi_awsize),
+        .s_axi_awburst(s_axi_awburst),
+        .s_axi_awlock(s_axi_awlock),
+        .s_axi_awcache(s_axi_awcache),
+        .s_axi_awprot(s_axi_awprot),
+        .s_axi_awvalid(s_axi_awvalid),
+        .s_axi_awready(s_axi_awready),
+        .s_axi_wdata(s_axi_wdata),
+        .s_axi_wstrb(s_axi_wstrb),
+        .s_axi_wlast(s_axi_wlast),
+        .s_axi_wvalid(s_axi_wvalid),
+        .s_axi_wready(s_axi_wready),
+        .s_axi_bid(s_axi_bid),
+        .s_axi_bresp(s_axi_bresp),
+        .s_axi_bvalid(s_axi_bvalid),
+        .s_axi_bready(s_axi_bready),
+        .s_axi_arid(s_axi_arid),
+        .s_axi_araddr(s_axi_araddr),
+        .s_axi_arlen(s_axi_arlen),
+        .s_axi_arsize(s_axi_arsize),
+        .s_axi_arburst(s_axi_arburst),
+        .s_axi_arlock(s_axi_arlock),
+        .s_axi_arcache(s_axi_arcache),
+        .s_axi_arprot(s_axi_arprot),
+        .s_axi_arvalid(s_axi_arvalid),
+        .s_axi_arready(s_axi_arready),
+        .s_axi_rid(s_axi_rid),
+        .s_axi_rdata(s_axi_rdata),
+        .s_axi_rresp(s_axi_rresp),
+        .s_axi_rlast(s_axi_rlast),
+        .s_axi_rvalid(s_axi_rvalid),
+        .s_axi_rready(s_axi_rready)
+    );
+
+
+endmodule
