0da35b8e48a4df62ad53f7dc59a47951704bb4d1
Timo Braungardt <timo.braungardt@web.de>
2026-09-22 11:38:35 +0200

    spi: add true memory helper class

---
+++ test/cocotb/HelperClasses/SpiFlashMemory.py
@@ -0,0 +1,103 @@
+import logging
+import cocotb
+from cocotbext.spi import SpiSlaveBase, SpiBus, SpiConfig
+from cocotb.triggers import FallingEdge, First, RisingEdge, Timer
+
+
+class SpiFlashMemory(SpiSlaveBase):
+    write_enable = 0x06
+    program = 0x02
+    read = 0x03
+    long_address_enable = 0xB7
+    write_any_address = 0x71
+
+    def __init__(self, bus):
+        self.log = logging.getLogger(f"cocotb.spi")
+        self._config = SpiConfig()
+        self.opcode = 0
+        self.address = 0
+        self.write_enable = False
+        self.quad_enable_bit = False
+        self.num_bytes = 4
+        self.address_width = 24
+        self._memory_size = 0x1000
+        self._generate_data_array()
+        super().__init__(bus)
+
+    def _generate_data_array(self):
+        self.data = [255 for x in range(self._memory_size)]
+
+    async def get_content(self):
+        await self.idle.wait()
+        return self.opcode, self.address
+
+    async def _recieve_data(self, num_bits: int) -> int:
+        rx_word = 0
+
+        frame_end = RisingEdge(self._cs) if self._config.cs_active_low else FallingEdge(self._cs)
+
+        for k in range(num_bits):
+            if (await First(RisingEdge(self._sclk), frame_end)) == frame_end or self._cs.value == 1:
+                raise RuntimeError("End of frame in the middle of a transaction")
+
+            rx_word |= int(self._mosi.value) << (num_bits - 1 - k)
+        return rx_word
+
+    async def _send_data(self, num_bits: int, tx_word: int) -> int:
+        frame_end = RisingEdge(self._cs) if self._config.cs_active_low else FallingEdge(self._cs)
+
+        for k in range(num_bits):
+            if (
+                await First(FallingEdge(self._sclk), frame_end)
+            ) == frame_end or self._cs.value == 1:
+                raise RuntimeError("End of frame in the middle of a transaction")
+
+            self._miso.value = bool(tx_word & (1 << (num_bits - 1 - k)))
+
+    async def _transaction(self, frame_start, frame_end):
+        await frame_start
+        self.log.info("SPI transaction started!")
+        self.idle.clear()
+        self.opcode = int(await self._recieve_data(8))
+        self.log.info("   opcode:  %x", self.opcode)
+        if self.opcode == SpiFlashMemory.write_enable:
+            self.write_enable = True
+            self.log.info("   writing enabled")
+        elif self.opcode == SpiFlashMemory.long_address_enable:
+            self.address_width = 32
+            self.log.info("   address length now 32 bit")
+        else:
+            self.address = int(await self._recieve_data(self.address_width))
+            self.log.info("   address: %d", self.address)
+
+        # Manager ordered a read
+        if self.opcode == SpiFlashMemory.read:
+            if (self.address + self.num_bytes) > self._memory_size:
+                raise RuntimeError("Access out of memory array")
+
+            for i in range(self.num_bytes):
+                data = self.data[self.address + i]
+                await self._send_data(8, data)
+                self.log.info("   sending %x", data)
+
+        # Manager ordered a program
+        if self.opcode == SpiFlashMemory.program:
+            if not self.write_enable:
+                raise RuntimeError("Write enable not set!")
+
+            for i in range(self.num_bytes):
+                data = int(await self._recieve_data(8))
+                self.log.info("   recieved %x", data)
+                self.data[self.address + i] = data
+
+        # Manager ordered a write to config registers
+        if self.opcode == SpiFlashMemory.write_any_address:
+            if not self.write_enable:
+                raise RuntimeError("Write enable not set!")
+
+            data = int(await self._recieve_data(8))
+            self.log.info(f"   recieved %x for register address %x", data, self.address)
+            if data == 0b00000010:
+                self.quad_enable_bit = True
+
+        await frame_end
--- test/cocotb/HelperClasses/__init__.py
+++ test/cocotb/HelperClasses/__init__.py
@@ -1,3 +1,4 @@
 from .SpiFlashFiFo import SpiFlashFiFo
+from .SpiFlashMemory import SpiFlashMemory
 from .QSpiFlashFiFo import QSpiFlashFiFo
 from .DummyData import DummyData
